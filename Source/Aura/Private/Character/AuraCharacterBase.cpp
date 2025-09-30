bc32b81384623bd3ffa112d96ad22d7e25e2db5f
Juan Quiroz <explodingfire@gmail.com>
2025-09-30 02:06:26 -0700

    tutorial Section 2 complete

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,21 @@
+// Copyright Juan Quiroz
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Juan Quiroz
+
+
+#include "Game/AuraGameModeBase.h"
+
